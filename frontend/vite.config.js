142c438930b0765d50e6dcabebd9a6e8e9a9be87
Huijin Yu <huijin.yu@westernacher.com>
2026-01-26 09:24:31 +0800

    change code in api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,8 +13,9 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
